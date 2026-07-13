a2164cf884c6b030b71ba8f1922a3c254929b78d
Tarz <tarzsardana@gmail.com>
2026-07-14 00:05:06 +0530

    Added task 2

---
+++ Task2/main.py
@@ -0,0 +1,96 @@
+import streamlit as st
+from langchain_helper import (
+    create_vector_db,
+    get_qa_chain,
+    search_web,
+    update_knowledge_base,
+    analyze_image
+)
+
+# ---------------------------------
+# Page Configuration
+# ---------------------------------
+st.set_page_config(
+    page_title="Customer Service Chatbot",
+    page_icon="🤖"
+)
+
+st.title("🤖 CUSTOMER SERVICE CHATBOT")
+st.subheader("📷 Image Upload")
+
+uploaded_image = st.file_uploader(
+    "Upload an image",
+    type=["jpg", "jpeg", "png"]
+)
+
+if uploaded_image:
+    st.image(uploaded_image)
+# ---------------------------------
+# Create Knowledge Base
+# ---------------------------------
+if st.button("Create Knowledgebase"):
+
+    with st.spinner("Creating Knowledge Base..."):
+
+        create_vector_db()
+
+    st.success("Knowledge Base Created Successfully!")
+
+# ---------------------------------
+# Ask Question
+# ---------------------------------
+question = st.text_input("Ask your question")
+
+if question:
+  if uploaded_image:
+
+        with st.spinner("Analyzing Image..."):
+
+            answer = analyze_image(
+                uploaded_image,
+                question
+            )
+
+        st.subheader("Image Analysis")
+
+        st.write(answer)
+
+        st.stop()
+  with st.spinner("Searching..."):
+
+     chain = get_qa_chain()
+
+     response = chain.invoke({"input": question})
+
+     answer = response["answer"].strip()
+    
+    
+
+    # ---------------------------------
+    # If answer not found in FAISS
+    # ---------------------------------
+     if "don't know" in answer.lower():
+
+        st.info("Searching the web...")
+
+        web_answer = search_web(question)
+
+        if web_answer:
+
+            # Save in knowledge_updates.csv
+            update_knowledge_base(question, web_answer)
+
+            answer = web_answer
+
+            st.success("Knowledge Base Updated Successfully!")
+
+        else:
+
+            answer = "Sorry, I couldn't find an answer."
+
+    # ---------------------------------
+    # Display Final Answer
+    # ---------------------------------
+     st.subheader("Answer")
+
+     st.write(answer)
